8c7ea0d202bb5fc7d56f2c304a2434d46a6f4d1d
Will McCutchen <will@mccutch.org>
2024-02-05 23:14:37 -0500

    Add tests

---
+++ safedialer_example_test.go
@@ -0,0 +1,48 @@
+package safedialer_test
+
+import (
+	"fmt"
+	"net"
+	"net/http"
+
+	"github.com/mccutchen/safedialer"
+)
+
+// Example demonstrates basic usage and functionality of safedialer.Control.
+func Example() {
+	client := &http.Client{
+		Transport: &http.Transport{
+			DialContext: (&net.Dialer{
+				Control: safedialer.Control,
+			}).DialContext,
+		},
+	}
+
+	urls := []string{
+		// safe request to 3rd party web site
+		"https://httpbingo.org/status/201",
+
+		// unsafe request, resolves to an internal IP address that might expose
+		// sensitive information about your infrastructure
+		"http://www.10.0.0.1.nip.io",
+	}
+	for _, url := range urls {
+		resp, err := client.Get(url)
+		printResponse(url, resp, err)
+	}
+
+	// Output:
+	// https://httpbingo.org/status/201
+	// ✅ 201 Created
+	//
+	// http://www.10.0.0.1.nip.io
+	// ❌ Get "http://www.10.0.0.1.nip.io": dial tcp 10.0.0.1:80: 10.0.0.1 is not a public IP address
+}
+
+func printResponse(url string, resp *http.Response, err error) {
+	if err != nil {
+		fmt.Printf("%s\n❌ %s\n\n", url, err)
+	} else {
+		fmt.Printf("%s\n✅ %s\n\n", url, resp.Status)
+	}
+}
